7043445a055d72a3088eb007116ebd6e5b57d725
Adwin Lorance <adwin.lorance@gmail.com>
2025-06-01 22:40:42 -0400

    attempting again

--- application.py
+++ application.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the app directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
+
 from app import app
 
 # This is the file that Oryx looks for by default
